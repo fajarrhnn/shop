69be6d31ff5b739c3a39230b7e0e6ae1d5a47b9f
rehankvk <fajarrehan023@gmail.com>
2024-05-22 15:32:55 +0700

    adding user types

--- src/lib/definition.ts
+++ src/lib/definition.ts
@@ -1,13 +1,22 @@
 interface Products {
-    id: number,
+    id: string,
     title: string,
     description: string,
     image: string,
-    price: number,
     category: string,
-    rating: {
-        rate: string,
-        count: number
-    }
+    slug: string,
+    price: number,
+    rating: number,
+    stock: number
 }
 export type { Products as ProductsTypes }
+
+interface Users {
+    id: string,
+    firstName: string,
+    lastName: string,
+    email?: string,
+    password?: string
+}
+
+export type { Users as UsersTypes }
