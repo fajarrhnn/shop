b24042c21be7dd06baa54fc925163ac6439b360a
rehankvk <fajarrehan023@gmail.com>
2024-05-22 15:34:19 +0700

    Create a product api and change the api for its product data

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,39 +1,49 @@
-import { ProductsTypes } from "@/lib/definition"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import Image from "next/image"
-import { formatter } from "@/lib/utils"
-import Link from "next/link"
+import { ProductsTypes } from "@/lib/definition";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+import { formatter } from "@/lib/utils";
+import Link from "next/link";
 
 async function getData() {
-    const res = await fetch('https://fakestoreapi.com/products')
+    const res = await fetch("/api/product", {
+        method: "GET",
+    });
     if (!res.ok) {
-        throw new Error('Failed to fetch data')
+        throw new Error("Failed to fetch data");
     }
-
-    return res.json()
+    return res.json();
 }
 
 export default async function ProductsPage() {
-    const data: ProductsTypes[] = await getData()
-
+    const data = await getData();
+    const products: ProductsTypes[] = data.result.rows
+    console.log(products)
 
     return (
         <>
-            <main className="w-11/12 mx-auto py-4">
-                <section className="flex flex-col md:grid md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {data?.map((item: ProductsTypes) => (
-                        <Card className="rounded-none shadow-none border-none" key={item.id}>
-                            <CardHeader className="h-56 flex justify-center items-center">
-                                <Image src={item.image} alt={item.title} width={120} height={120} className="w-auto h-auto object-contain mx-auto" />
-                            </CardHeader>
-                            <CardContent>
-                                <Link href={`/products/${item.id}`}><h1 className="text-sm line-clamp-2">{item.title}</h1></Link>
-                                <p className="text-base font-bold">{formatter.format(item.price)}</p>
-                            </CardContent>
-                        </Card>
-                    ))}
-                </section>
-            </main>
+            <section className="flex flex-col md:grid md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {products?.map((item: ProductsTypes) => (
+                    <Card className="rounded-none shadow-none border-none" key={item.id}>
+                        <CardHeader className="h-56 flex justify-center items-center">
+                            <Image
+                                src={item.image}
+                                alt={item.title}
+                                width={120}
+                                height={120}
+                                className="w-auto h-auto object-contain mx-auto"
+                            />
+                        </CardHeader>
+                        <CardContent>
+                            <Link href={`/products/${item.id}`}>
+                                <h1 className="text-sm line-clamp-2">{item.title}</h1>
+                            </Link>
+                            <p className="text-base font-bold">
+                                {formatter.format(item.price)}
+                            </p>
+                        </CardContent>
+                    </Card>
+                ))}
+            </section>
         </>
-    )
+    );
 }
