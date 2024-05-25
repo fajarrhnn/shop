514bd273d7c86f2fc0ef1e17bd889703e16b2ef3
rehankvk <fajarrehan023@gmail.com>
2024-05-26 05:42:14 +0700

    create login and register pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,11 @@
 export default function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <>
-            <main className="w-11/12 mx-auto min-h-screen">
-                <section className="flex justify-center items-center">
-                    {children}
-                </section>
-            </main>
-        </>
-    )
-}
+      <>
+        <main className="w-11/12 mx-auto min-h-screen">
+          <section className="flex justify-center items-center">
+            {children}
+          </section>
+        </main>
+      </>
+    );
+  }
