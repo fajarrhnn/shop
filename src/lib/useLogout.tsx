23b49b72d3561243a565d27c2e2ee31fc633f78c
rehankvk <fajarrehan023@gmail.com>
2024-05-26 05:25:45 +0700

    create function useLogout

--- src/lib/useLogout.tsx
+++ src/lib/useLogout.tsx
@@ -1,22 +1,27 @@
-import { useRouter } from "next/router";
-import { useState } from "react";
+"use client";
 
-export const useLogout = async () => {
-    const [user, setUser] = useState(null)
-    const router = useRouter()
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
+
+export function useLogout() {
+  const router = useRouter();
+
+  const logout = useCallback(async () => {
     try {
-        const res = await fetch('/api/protect', {
-            method: 'POST',
-        });
-        const data = await res.json();
+      const res = await fetch("/api/protect", {
+        method: "POST",
+      });
+      const data = await res.json();
 
-        if (data.success) {
-            setUser(null);
-            router.push('/login');
-        } else {
-            console.error('Failed to logout');
-        }
+      if (data.success) {
+        router.push("/login");
+      } else {
+        console.error("Failed to logout");
+      }
     } catch (error) {
-        console.error('Error during logout:', error);
+      console.error(error);
     }
-};
+  }, [router]);
+
+  return logout;
+}
