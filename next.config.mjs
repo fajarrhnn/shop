52984887be816dd6e73cd153884b701340896504
rehankvk <fajarrehan023@gmail.com>
2024-05-28 04:06:48 +0700

    add a configuration cors header

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/img/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
           {
-            protocol: 'https',
-            hostname: 'fakestoreapi.com',
-            port: '',
-            pathname: '/img/**',
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
           },
         ],
       },
+    ];
+  },
 };
 
 export default nextConfig;
