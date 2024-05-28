276d22ffda7deba950de0c50616a491d27423e94
rehankvk <fajarrehan023@gmail.com>
2024-05-28 07:59:14 +0700

    Delete item from cart

---
+++ src/app/api/cart/[id]/route.ts
@@ -0,0 +1,29 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function DELETE(req: NextRequest) {
+  const BearerToken = req.headers.get("Authorization");
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop();
+
+  try {
+    if (!BearerToken) {
+      return NextResponse.json({ message: "Unauthorized!!" }, { status: 401 });
+    }
+
+    const result = await sql`DELETE FROM Carts WHERE id= ${id}`;
+    if (!result.rows || result.rowCount === 0) {
+      return NextResponse.json(
+        { message: "No data in the cart." },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Successfully remove data in the cart.", result },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
