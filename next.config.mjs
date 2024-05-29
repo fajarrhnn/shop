628755412d29bea0673ae9d80ef9c72ab0192c5a
rehankvk <fajarrehan023@gmail.com>
2024-05-30 06:27:00 +0700

    setting value Access-Control-Allow-Origin

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://jarot-shop.vercel.app"
+            value: "*"
           },
           {
             key: "Access-Control-Allow-Methods",
